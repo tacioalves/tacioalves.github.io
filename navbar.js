526a991c18ff1c6f9e84e1194010d9332260e8cc
tacioalves <tacio.alv@gmail.com>
2023-07-28 21:12:42 -0300

    NAVBAR

---
+++ navbar.js
@@ -0,0 +1,14 @@
+//Multiple options dropdown
+//https://codepen.io/gatoledo1/pen/QWmpWjK
+
+const optionMenu = document.querySelector(".select-menu"),
+  selectBtn = optionMenu.querySelector(".select-btn"),
+  options = optionMenu.querySelectorAll(".option"),
+  sBtn_text = optionMenu.querySelector(".sBtn-text");
+
+selectBtn.addEventListener("click", () =>
+  optionMenu.classList.toggle("active")
+);
+
+
+;
